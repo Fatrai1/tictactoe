6c001217c02a5aca0f059f5787436e5e4c9e6948
Fatrai2 <midgard@citromail.hu>
2020-11-26 20:50:05 +0100

    javított

--- js/tictactoe.js
+++ js/tictactoe.js
@@ -1,17 +1,10 @@
-'use strict';
-// maga a mátrix, egyelőre csak egy üres tömb
 const matrix = [];
-// sorok és oszlopok száma, hátha nem négyzetes
 const rows = 3;
 const cols = 3;
-// lépések száma
 let stepCount = 0;
-// az aktuális jel
-let mark = 'X'
+let mark = 'X';
 
-// csak feltöltöm a mátrixot (valjában opcionális lépés is lehet)
 const initState = () => {
-    // ehelyett a fill metódussal szebb lenne
     for (let i = 0; i < rows; i += 1) {
         matrix[i] = [];
         for (let j = 0; j < cols; j += 1) {
@@ -20,26 +13,33 @@ const initState = () => {
     }
 }
 
-// a mátrix egy elemének értéket adok, az adott elem data attrinutumait 
-// felhasználva nyerem ki az értéket
+const deleteSigns = () => {
+    document
+        .querySelectorAll('.tictactoe__cell')
+        .forEach(element => {
+            element.textContent = '';
+        });
+}
+
 const changeMatrixValue = (element) => {
     const row = parseInt(element.dataset.row, 10);
     const cell = parseInt(element.dataset.cell, 10);
     matrix[row][cell] = element.textContent;
 }
 
-const setMark = () => {
-    mark = mark === 'X' ? 'O' : 'X';
-}
 const increaseCounter = () => {
     stepCount += 1;
 }
+
 const modifyCell = (element) => {
     element.removeEventListener('click', handleClick);
     element.textContent = mark;
 }
 
-// kattintáskor mi történjen, érdemes lenne több függvényre bontani
+const setMark = () => {
+    mark = mark === 'X' ? 'O' : 'X';
+}
+
 const handleClick = (event) => {
     increaseCounter();
     modifyCell(event.target);
@@ -48,53 +48,88 @@ const handleClick = (event) => {
     checkWinner();
 }
 
-// minden elemhez hozzáadom az eseményfigyelőt
-const addListener = () => {
-    document.querySelectorAll('.tictactoe__cell').forEach(element => {
-        element.addEventListener('click', handleClick)
-    });
+const addClickListener = () => {
+    document
+        .querySelectorAll('.tictactoe__cell')
+        .forEach(element => {
+            element.addEventListener('click', handleClick)
+        });
 }
 
-// ha van győrztes minden elemről eltávolítom az eseményfigyelőt
-const removeListener = () => {
-    document.querySelectorAll('.tictactoe__cell').forEach(element => {
-        element.removeListener('click', handleClick)
-    });
+const removeAllClickListener = () => {
+    document
+        .querySelectorAll('.tictactoe__cell')
+        .forEach(element => {
+            element.removeEventListener('click', handleClick)
+        });
 }
 
-// Megnézem hogy van e olyan sor, ahol minden elem ugyanaz
-const checkRowValues = () => {
-    const values = matrix.map(row =>
-        row.every((value) => value === 'X') ||
-        row.every((value) => value === 'O'))
-    return values.indexOf(true) !== -1 ? true : false;
+const checkValues = (arr) => {
+    for (let i = 0; i < arr.length; i += 1) {
+        if (
+            arr[i].every((value) => value === 'X') ||
+            arr[i].every((value) => value === 'O')
+        ) {
+            return true;
+        }
+    }
+    return false
 }
 
-// Megnézem hogy van e olyan oszlop, ahol minden elem ugyanaz
-// TODO: Meg kell írnod, boolean adjon vissza
 const checkColumnValues = () => {
-   const colums =[];
-   for(let i=0; i < matrix.length; i++) {
-       colums[i] =[];
-       for(let j=0; j<matrix[i].length; j++) {
-           colums[i][j] = matrix[j][i];
-       }
-   }
- }
-
-// Megnézem hogy van e olyan oszlop, ahol minden elem ugyanaz
-// TODO: Meg kell írnod, boolean adjon vissza
-const checkDiagonalValues = () => { 
-
+    const columns = [];
+    for (let i = 0; i < matrix.length; i += 1) {
+        columns[i] = [];
+        for (let j = 0; j < matrix[i].length; j++) {
+            columns[i][j] = matrix[j][i];
+        }
+    }
+    return checkValues(columns);
 }
 
+const checkDiagonalValues = () => {
+    const firstDiagonal = [];
+    const secondDiagonal = [];
+    for (let i = 0; i < matrix.length; i += 1) {
+        firstDiagonal.push(matrix[i][i]);
+        secondDiagonal.push(matrix[i][matrix[i].length - i - 1])
+    }
+    return checkValues([firstDiagonal, secondDiagonal]);
+}
 
-// TODO: Meg kell írnod, nincs befejezve
 const checkWinner = () => {
-    // Akár a checkRowValues, checkColumnValues, checkDiagonalValues true, akkor van győztes
-    // Csak azért van itt a log hogy lássátok hogy true akkor lesz ha van olyan sor ahol minden elem ugyanaz
-    console.log(checkRowValues());
+    if (checkValues(matrix) || checkColumnValues() || checkDiagonalValues()) {
+        endGame()
+    }
 }
 
-initState();
-addListener();
+const setMessage = (message) => {
+    document
+        .querySelector('.message')
+        .textContent = message
+}
+
+const startGame = () => {
+    initState();
+    addClickListener();
+    newGame();
+}
+
+const endGame = () => {
+    setMessage('A győztes: ' + (mark === 'X' ? 'O' : 'X'));
+    removeAllClickListener();
+}
+
+const newGame = () => {
+    document
+        .querySelector('.new-game')
+        .addEventListener('click', () => {
+            initState();
+            addClickListener();
+            deleteSigns();
+            setMessage('');
+            setMark()
+        })
+}
+
+startGame();
